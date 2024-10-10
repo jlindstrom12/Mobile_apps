796155b1ae6e2d478d78de092cff818b07a003e0
Juan Pablo Urista <jurista@andrew.cmu.edu>
2024-10-10 15:45:06 -0400

    Initial Commit

---
+++ Viator/ViatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViatorApp.swift
+//  Viator
+//
+//  Created by Juan Pablo Urista on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ViatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
