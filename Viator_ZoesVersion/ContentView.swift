dc61ca3b604ba70f08a757fa7cc486dbff78afa3
Zoe Angell <you@example.com>
2024-10-19 14:15:37 -0400

    Initial Commit

---
+++ Viator_ZoesVersion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Viator_ZoesVersion
+//
+//  Created by Zoe Angell on 10/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Viator_ZoesVersion/Viator_ZoesVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Viator_ZoesVersionApp.swift
+//  Viator_ZoesVersion
+//
+//  Created by Zoe Angell on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Viator_ZoesVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
